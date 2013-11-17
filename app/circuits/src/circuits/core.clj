f77fd4a1509b4a86229cab722c4711dfadf06735
Koleman Nix <kolemannix@gmail.com>
2013-11-17 06:31:50 -0500

    fix for register output field (q instead of data) also fix id issue

--- app/circuits/src/circuits/core.clj
+++ app/circuits/src/circuits/core.clj
@@ -11,7 +11,7 @@
 (defn generate-id [species circuit]
   (let [same-species (filter #(= (% :species) species) (vals circuit))
         same-count (count same-species)]
-    (str species same-count))) 
+    (keyword (str species same-count)))) 
 
 (declare function-map gen-inputs)
 
@@ -33,6 +33,7 @@
 (defn add-component [species circuit display]
   (let [new-id (generate-id species circuit)
         new-component (build/build-component species new-id)
+        asdf (println new-component)
         with-display (assoc new-component :display display)
         new-state (assoc circuit new-id with-display)
         ]
@@ -74,12 +75,12 @@
     new-circuit))
 
 ;; External Interface Functions
-(defn add-component-js [species circuit display]
-  (clj->js (add-component species (js->clj circuit) (js->clj display))))
-(defn add-connection-js [src dst circuit]
-  (clj->js (add-connection (js->clj src) (js->clj dst) (js->clj circuit)))) 
-(defn remove-connection-js [src dst circuit]
-  )
+;(defn add-component-js [species circuit display]
+  ;(clj->js (add-component species (js->clj circuit) (js->clj display))))
+;(defn add-connection-js [src dst circuit]
+  ;(clj->js (add-connection (js->clj src) (js->clj dst) (js->clj circuit)))) 
+;(defn remove-connection-js [src dst circuit]
+  ;)
 ;; End External Interface FNs
 
 (defn inner-fn  [mapping]
